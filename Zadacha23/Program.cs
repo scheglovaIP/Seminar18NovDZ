6089f01ec34f48636170536c9184f85dae17b5cf
Irina <ip_scheglova@mail.ru>
2022-11-19 14:01:35 +0500

    add Home Work Number 23

---
+++ Zadacha23/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+} 
+
+int number = ReadInt("Введите число: ");
+int i = 1;
+
+if(number<1)
+{
+    while(number<i)
+    {
+        Console.Write(i*i*i + ", ");
+        i--;
+    }
+}
+else if(number>1)
+{
+    while(i<number)
+    {
+        Console.Write(i*i*i + ", ");
+        i++;
+    }
+}
+Console.WriteLine(i*i*i);
